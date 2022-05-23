8eaaf9094a35d2cafc607d7a8734cc76e84c9984
parli <parlikareshaan@gmail.com>
2022-05-23 08:52:16 -0700

    hashmaps for player speeds

---
+++ src/main/java/com/nitrocode/db/score/Scoreboard.java
@@ -0,0 +1,11 @@
+package com.nitrocode.db.score;
+
+import java.util.Map;
+import java.util.Set;
+
+public class Scoreboard {
+
+    private Map<Integer, Integer> playertoSpeedMapping;
+    private Map<Integer, Set<Integer>> speedtoPlayerMapping;
+
+}
