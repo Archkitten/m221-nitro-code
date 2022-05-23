3b9f7e20895e370c7e4dfe0ef6c602f34104651e
parli <parlikareshaan@gmail.com>
2022-05-23 08:54:23 -0700

    hashmaps for player speeds

--- src/main/java/com/nitrocode/db/score/Scoreboard.java
+++ src/main/java/com/nitrocode/db/score/Scoreboard.java
@@ -1,11 +1,15 @@
 package com.nitrocode.db.score;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Scoreboard {
 
     private Map<Integer, Integer> playertoSpeedMapping;
     private Map<Integer, Set<Integer>> speedtoPlayerMapping;
+
+    public Scoreboard() {
+        this.playertoSpeedMapping = new HashMap<>();
+        this.speedtoPlayerMapping = new TreeMap<>(Collections.reverseOrder());
+    }
 
 }
