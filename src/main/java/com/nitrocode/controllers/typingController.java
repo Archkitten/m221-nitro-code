cd77cef828cc667a3cbcb572863699fd8dbce132
Arch Huang <Archkitten@users.noreply.github.com>
2022-06-01 09:20:46 -0700

    Typing Speed - Tab indent and 'backend' - Will prevent tab from being hit

--- src/main/java/com/nitrocode/controllers/typingController.java
+++ src/main/java/com/nitrocode/controllers/typingController.java
@@ -9,7 +9,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class typingController {
     @GetMapping("/typing")
+
     public String type(Model model) {
+        String[] texts = {"public class Main {\npublic static void main(String[] args) {\nSystem.out.println('Hello World');\n}\n}",
+                "System.out.println('Hello World!');",
+                "if (20 > 18) {\nSystem.out.println('20 is greater than 18');\n}",
+                "for (int i = 0; i < 5; i++) {\nSystem.out.println(i);\n}"};
+        int randomNumber = (int)(Math.random() * texts.length);
+        String text = texts[randomNumber];
+
+        model.addAttribute("html", text);
         return "typing";
     }
 }
