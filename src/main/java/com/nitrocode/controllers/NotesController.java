e66f80c03f7d6535d9aa36197d02afacc48fafe1
Arch Huang <Archkitten@users.noreply.github.com>
2022-06-06 09:14:25 -0700

    Notes...

--- src/main/java/com/nitrocode/controllers/NotesController.java
+++ src/main/java/com/nitrocode/controllers/NotesController.java
@@ -1,24 +1,35 @@
 package com.nitrocode.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.nitrocode.notes.*;
+import com.nitrocode.notes.ScrumViewController;
+import com.nitrocode.notes.NoteViewController;
 
 import java.util.List;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class NotesController {
+    @Autowired
+    private NoteJpaRepository noteRepository;
+    @Autowired
+    private ModelRepository modelRepository;
+
     @GetMapping("/database/notes/{id}")
     public String notes(@PathVariable("id") Long id, Model model) {
+
+        NoteViewController mmm = new NoteViewController();
+
         Person person = modelRepository.get(id);
         List<Note> notes = noteRepository.findAllByPerson(person);
         Note note = new Note();
         note.setPerson(person);
 
         for (Note n : notes)
-            n.setText(convertMarkdownToHTML(n.getText()));
+            n.setText(mmm.convertMarkdownToHTML(n.getText()));
 
         model.addAttribute("person", person);
         model.addAttribute("notes", notes);
